59336fd7baeecd5713492bb78b4f77277e0290c0
edcombs <evandcombs@deaddodoproductions.com>
2015-10-01 22:33:52 -0400

    Lots of work little progress, found best way to approach this.

--- ResourcesConvert/MainWindow.xaml.cs
+++ ResourcesConvert/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Controls.Primitives;
 
 namespace ResourcesConvert
 {
@@ -20,14 +21,75 @@ namespace ResourcesConvert
     /// </summary>
     public partial class MainWindow : Window
     {
+        private ResourceManager resourceManager;
+
         public MainWindow()
         {
             InitializeComponent();
+            resourceManager = new ResourceManager();
+            dataGrid.ItemsSource = resourceManager.Resources;
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            switch ((string)button.Tag)
+            {
+                case "new":
+                    break;
+                case "open":
+                    break;
+                case "save":
+                    break;
+                case "convert":
+                    break;
+                case "add_column":
+                    dataGrid.ItemsSource = resourceManager.AddColumn(textBox.Text);
+                    textBox.Clear();
+                    break;
+                case "add_row":
+                    resourceManager.AddResourceItem();
+                    break;
+            }
+        }
+        private static T GetVisualChild<T>(Visual parent) where T : Visual
+        {
+            T child = default(T);
+            int numVisuals = VisualTreeHelper.GetChildrenCount(parent);
+            for (int i = 0; i < numVisuals; i++)
+            {
+                Visual v = (Visual)VisualTreeHelper.GetChild(parent, i);
+                child = v as T;
+                if (child == null) child = GetVisualChild<T>(v);
+                if (child != null) break;
+            }
+            return child;
         }
 
-        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void dataGrid_CellEditEnding(object sender, DataGridCellEditEndingEventArgs e)
         {
+            DataGrid dataGrid = (DataGrid)sender;
+            DataGridRow dataGridRow = e.Row;
+            if (dataGridRow != null)
+            {
+                DataGridCellsPresenter presenter = GetVisualChild<DataGridCellsPresenter>(dataGridRow);
+                DataGridCell cell = (DataGridCell)presenter.ItemContainerGenerator.ContainerFromIndex(0);
+                if (cell != null)
+                {
+                    DataGridColumn column = e.Column;
+                    string text = ((TextBox)cell.Content).Text;
+                    int row = dataGridRow.GetIndex();
+                    string language = (string)column.Header;
+                    //resourceManager.ChangeResource(row, language, text);
+                }
+            }
+        }
 
+        private void dataGrid_LostFocus(object sender, RoutedEventArgs e)
+        {
+            DataGrid dataGrid = (DataGrid)sender;
+            DataGridCellsPresenter presenter = GetVisualChild<DataGridCellsPresenter>(dataGrid);
+            DataGridCell cell = (DataGridCell)presenter.ItemContainerGenerator.ContainerFromIndex(0);
+            cell.IsEditing = false;
         }
     }
 }
---
+++ ResourcesConvert/ResourceTypeGenerator.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace ResourcesConvert
+{
+    public static class ResourceTypeGenerator
+    {
+        public static dynamic CreateResourceObject(Type type)
+        {
+            return Activator.CreateInstance(type);
+        }
+        public static Type CreateResourceType(List<Property> list)
+        {
+            TypeBuilder tb = GetTypeBuilder();
+            ConstructorBuilder constructor = tb.DefineDefaultConstructor(MethodAttributes.Public | MethodAttributes.SpecialName | MethodAttributes.RTSpecialName);
+
+            foreach (Property property in list)
+            {
+                CreateProperty(tb, property.Name, property.Type);
+            }
+            Type objectType = tb.CreateType();
+            return objectType;
+        }
+        private static TypeBuilder GetTypeBuilder()
+        {
+            string signature = "signature";
+            AssemblyName assemblyName = new AssemblyName(signature);
+            AssemblyBuilder assemblyBuilder = AppDomain.CurrentDomain.DefineDynamicAssembly(assemblyName, AssemblyBuilderAccess.Run);
+            ModuleBuilder moduleBuilder = assemblyBuilder.DefineDynamicModule("MainModule");
+            TypeBuilder typeBuilder = moduleBuilder.DefineType(signature, TypeAttributes.Public | TypeAttributes.Class | TypeAttributes.AutoClass | TypeAttributes.AnsiClass | TypeAttributes.BeforeFieldInit | TypeAttributes.AutoLayout, null);
+            return typeBuilder; 
+        }
+        private static void CreateProperty(TypeBuilder typeBuilder, string propertyName, Type propertyType)
+        {
+            FieldBuilder fieldBuilder = typeBuilder.DefineField(propertyName.ToUpper(), propertyType, FieldAttributes.Private);
+            PropertyBuilder propertyBuilder = typeBuilder.DefineProperty(propertyName, PropertyAttributes.HasDefault, propertyType, null);
+            MethodBuilder getMethodBuilder = typeBuilder.DefineMethod("Get" + propertyName, MethodAttributes.Public | MethodAttributes.SpecialName | MethodAttributes.HideBySig, propertyType, Type.EmptyTypes);
+            ILGenerator GetIlGenerator = getMethodBuilder.GetILGenerator();
+
+            GetIlGenerator.Emit(OpCodes.Ldarg_0);
+            GetIlGenerator.Emit(OpCodes.Ldfld, fieldBuilder);
+            GetIlGenerator.Emit(OpCodes.Ret);
+
+            MethodBuilder setMethodBuilder = typeBuilder.DefineMethod("Set" + propertyName, MethodAttributes.Public | MethodAttributes.SpecialName | MethodAttributes.HideBySig, null, new[] { propertyType });
+            ILGenerator setIlGenerator = setMethodBuilder.GetILGenerator();
+            Label modifyProperty = setIlGenerator.DefineLabel();
+            Label exitSet = setIlGenerator.DefineLabel();
+
+            setIlGenerator.MarkLabel(modifyProperty);
+            setIlGenerator.Emit(OpCodes.Ldarg_0);
+            setIlGenerator.Emit(OpCodes.Ldarg_1);
+            setIlGenerator.Emit(OpCodes.Stfld, fieldBuilder);
+
+            setIlGenerator.Emit(OpCodes.Nop);
+            setIlGenerator.MarkLabel(exitSet);
+            setIlGenerator.Emit(OpCodes.Ret);
+
+            propertyBuilder.SetGetMethod(getMethodBuilder);
+            propertyBuilder.SetSetMethod(setMethodBuilder);
+        }
+    }
+    public class Property
+    {
+        public string Name { get; set; }
+        public Type Type { get; set; }
+        public Property() { }
+        public Property(string name, Type type)
+        {
+            Name = name;
+            Type = type;
+        }
+    }
+}
